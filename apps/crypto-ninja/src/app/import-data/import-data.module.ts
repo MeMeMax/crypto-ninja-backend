400b4f0322e7b9ff4bd4a12cdde24f51925fe198
Maximilian Ridder <maximilianridder@web.de>
2021-12-21 14:27:29 +0100

    provide holder controller and service

--- apps/crypto-ninja/src/app/import-data/import-data.module.ts
+++ apps/crypto-ninja/src/app/import-data/import-data.module.ts
@@ -1,5 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
-@Module({ imports: [HttpModule] })
+import { HolderController } from './controller/holder/holder.controller';
+import { HolderService } from './services/holder/holder.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [HolderController],
+  providers: [HolderService],
+})
 export class ImportDataModule {}
