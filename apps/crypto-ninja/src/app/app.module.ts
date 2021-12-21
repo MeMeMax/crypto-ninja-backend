e9104632acc020d94a6fb4128048d7d42ee7feb5
Maximilian Ridder <maximilianridder@web.de>
2021-12-21 11:24:13 +0100

    add import-data module

--- apps/crypto-ninja/src/app/app.module.ts
+++ apps/crypto-ninja/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImportDataModule } from './import-data/import-data.module';
 
 @Module({
-  imports: [],
+  imports: [ImportDataModule],
   controllers: [AppController],
   providers: [AppService],
 })
