4d2291664ebef53c48403b2cd16b66684b8d7881
Maximilian Ridder <maximilianridder@web.de>
2021-12-21 11:38:49 +0100

    add database connection

--- apps/crypto-ninja/src/app/app.module.ts
+++ apps/crypto-ninja/src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImportDataModule } from './import-data/import-data.module';
 
 @Module({
-  imports: [ImportDataModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'crypto-ninja',
+      password: 'crypto-ninja112358',
+      database: 'crypto-ninja',
+      bigNumberStrings: false,
+      cache: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      dateStrings: true,
+    }),
+    ImportDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
