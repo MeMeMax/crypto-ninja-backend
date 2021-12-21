affefea9a49db03dcb7a4fb035a4451fc7a65b68
Maximilian Ridder <maximilianridder@web.de>
2021-12-21 14:27:13 +0100

    add import endpoint for holder

--- apps/crypto-ninja/src/app/import-data/controller/holder/holder.controller.ts
+++ apps/crypto-ninja/src/app/import-data/controller/holder/holder.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { lastValueFrom } from 'rxjs';
+
+import { HolderService } from '../../services/holder/holder.service';
 
 @Controller('holder')
-export class HolderController {}
+export class HolderController {
+  constructor(private holderService: HolderService) {}
+
+  @Get('import')
+  async getStockDetails() {
+    const coins = (await lastValueFrom(this.holderService.getCoins())).data;
+
+    for (let coin of coins) {
+      if (coin.platforms && coin.platforms['binance-smart-chain']) {
+        const holder = (
+          await lastValueFrom(
+            this.holderService.getTokenHolder(
+              coin.platforms['binance-smart-chain']
+            )
+          )
+        ).data.data.items.length;
+
+        console.log(holder);
+      }
+    }
+  }
+}
